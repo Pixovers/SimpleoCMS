43c291bcf3185e4e2a5283cc7901c1f9374c0cb6
Samuele Girgenti <samuele.girgenti458@gmail.com>
2020-12-02 22:27:23 +0100

    Update
    Co-Authored-By: Bao500ct <63925908+Bao500ct@users.noreply.github.com>

---
+++ pages/control_panel/setup.php
@@ -0,0 +1,16 @@
+<?php
+
+if( isset( $_GET['submit'] ) ) {
+    
+} else {
+
+?>
+
+<form action="setup.php" method="GET">
+    <input type="text" name="email" placeholder="E-mail">
+    <button type="submit" name="submit">Create</button>
+</form>
+
+
+<?php } ?>
+
