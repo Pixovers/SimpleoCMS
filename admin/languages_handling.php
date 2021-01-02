7e7b20a6b6478c5a8472968a6f360c523d718470
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-01-02 23:31:02 +0100

    Languages Handling
    Co-Authored-By: Alessandro privitera <63925908+Bao500ct@users.noreply.github.com>

--- admin/languages_handling.php
+++ admin/languages_handling.php
@@ -19,8 +19,8 @@ if (isset($_ACTION)) {
 
         case "edit":
             if (isset($_GET['id'])) {
-                $lang = Language::byId( $_CONN, $_GET['id'] );
-                if( $lang ) {
+                $lang = Language::byId($_CONN, $_GET['id']);
+                if ($lang) {
                     if (isset($_POST['submit'])) {
                         $lang->setLangName($_POST['LangName']);
                         $lang->setLangCode($_POST['LangCode']);
@@ -37,6 +37,13 @@ if (isset($_ACTION)) {
             break;
 
         case "delete":
+            if (Language::delete($_CONN, $_GET['id'])) {
+                header("location: /admin/languages");
+                exit();
+            } else {
+                header("location: /admin/languages/edit/?id=".$_GET['id']."&error=delete");
+                exit();
+            }
             break;
 
         default:
@@ -73,31 +80,58 @@ if (isset($_ACTION)) {
                     <form method="POST">
                         <div class="card bg-light mt-3 mb-3 shadow-sm ">
                             <div class="card-header">
-                                <h5 class="card-title">New Language</h5>
+                                <h5 class="card-title">
+                                    <?php
+                                    if ($_ACTION == "new") echo "Create new language";
+                                    else echo "Edit language";
+                                    ?>
+                                </h5>
                             </div>
                             <div class="card-body">
                                 <div class="form-group col">
                                     <label for="LangName">Language name</label>
-                                    <input type="text" class="form-control" id="LangName" name="LangName" required>
+                                    <input type="text" class="form-control" id="LangName" name="LangName" value="<?php if ($_ACTION == "edit") echo $lang->getLangName(); ?>" required>
                                 </div>
 
                                 <div class="form-group col-4">
                                     <label for="LangCode">Language code</label>
-                                    <input type="text" class="form-control" id="LangCode" name="LangCode" required>
+                                    <input type="text" class="form-control" id="LangCode" name="LangCode" value="<?php if ($_ACTION == "edit") echo $lang->getLangCode(); ?>" required>
                                 </div>
                             </div>
                         </div>
 
                         <div class="text-right">
-                            <button class="btn btn-primary" type="submit" name="submit">Create</button>
+                            <button class="btn btn-primary" type="submit" name="submit">
+                                <?php
+                                if ($_ACTION == "new") echo "Create";
+                                else echo "Update";
+                                ?>
+                            </button>
+
+                            <?php if ($_ACTION == "edit") { ?>
+                                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteLangaugeModal">Delete</button>
+                                <div class="modal fade" id="deleteLangaugeModal" tabindex="-1" aria-labelledby="link" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Delete langauge</h5>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Are you sure you want to delete this language?</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" onclick="window.location.href='/admin/languages/delete/?id=<?php echo $lang->getLangId() ?>';">Yes, proceed</button>
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">no</button> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } ?>
+
                         </div>
                     </form>
                 </div>
                 <!--page content-->
             </div>
-
-
-
         </div>
     </div>
 
